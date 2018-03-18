77a99876f9667b1158e5d5f954c997a699e044ed
Sean Dwyer <sean.dwyer@gmail.com>
2018-03-17 17:46:24 -0700

    Fix social icons, wait for DOM load

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
-$('#buy-tickets-button').click((e) => {
-    e.preventDefault();
+$(() => {
+    $('#buy-tickets-button').click((e) => {
+        e.preventDefault();
 
-    $('html, body').animate({
-        scrollTop: $('#buy-tickets').offset().top,
-    }, 500);
+        $('html, body').animate({
+            scrollTop: $('#buy-tickets').offset().top,
+        }, 500);
+    });
 });
--- src/js/time-left.js
+++ src/js/time-left.js
@@ -1,23 +1,25 @@
 const eventDate = new Date('2018-05-18T18:00:00-0700');
 
-setInterval(() => {
-    const {days, hours, minutes, seconds} = timeLeft();
-    const pluralize = (number, label) => {
-        if (number === 1) {
-            return `${number} ${label}`;
-        }
-        return `${number} ${label}s`;
-    };
+$(() => {
+    setInterval(() => {
+        const {days, hours, minutes, seconds} = timeLeft();
+        const pluralize = (number, label) => {
+            if (number === 1) {
+                return `${number} ${label}`;
+            }
+            return `${number} ${label}s`;
+        };
 
-    if (days + hours + minutes + seconds > 0) {
-        $('.time-left').html(`
-            ${pluralize(days, 'day')} &nbsp;&nbsp;
-            ${pluralize(hours, 'hour')} &nbsp;&nbsp;
-            ${pluralize(minutes, 'minute')} &nbsp;&nbsp;
-            ${pluralize(seconds, 'second')}
-        `);
-    }
-}, 1000);
+        if (days + hours + minutes + seconds > 0) {
+            $('.time-left').html(`
+                ${pluralize(days, 'day')} &nbsp;&nbsp;
+                ${pluralize(hours, 'hour')} &nbsp;&nbsp;
+                ${pluralize(minutes, 'minute')} &nbsp;&nbsp;
+                ${pluralize(seconds, 'second')}
+            `);
+        }
+    }, 1000);
+});
 
 function timeLeft() {
     const now = new Date();
